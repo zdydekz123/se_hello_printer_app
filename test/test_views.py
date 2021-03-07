9f93504ba66e6f38305978d224cc870ded04f613
zdydekz123 <zdybek1997@gmail.com>
2021-03-07 13:55:25 +0000

    zad-9

--- test/test_views.py
+++ test/test_views.py
@@ -16,3 +16,7 @@ class FlaskrTestCase(unittest.TestCase):
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
         self.assertEqual(b'{ "imie":"Milosz", "mgs":Hello World!"}', rv.data)
+
+    def test_xml_with_output(self):
+        rv = self.app.get('/?output=xml')
+        self.assertEqual(b'<greetings><name>Milosz</name><msg>Hello World!</msg></greetings>', rv.data)
